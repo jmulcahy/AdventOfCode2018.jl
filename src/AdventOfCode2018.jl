078c74707fba93f7c7c3c02a9f921f137256b8fb
John Mulcahy <6724499+jmulcahy@users.noreply.github.com>
2018-12-08 16:09:42 -0700

    put day 4 in the package

--- src/AdventOfCode2018.jl
+++ src/AdventOfCode2018.jl
@@ -11,5 +11,6 @@ end
 include("Day1.jl")
 include("Day2.jl")
 include("Day3.jl")
+include("Day4.jl")
 
 end # module
--- src/Day4.jl
+++ src/Day4.jl
@@ -1,11 +1,7 @@
-using Dates
-import DataStructures.Stack
+module Day4
 
-function load(filename)
-    open(filename) do file
-        read(file, String)
-    end
-end
+import ..load_input
+using Dates
 
 function make_entry(time_string, message_string)
     dt = DateTime(time_string, "y-m-d H:M")
@@ -19,7 +15,8 @@ function make_entry(time_string, message_string)
     end
 end
 
-function parse_input(input)
+function parse_input()
+    input = load_input("4a")
     raw_entries = eachmatch(r"\[(.*)\] (.*)\n", input)
     parsed = [make_entry(re.captures[1], re.captures[2]) for re in raw_entries]
     sort(parsed, by=entry->entry.date_time)
@@ -45,7 +42,7 @@ function process_schedule(sched)
     guard_times
 end
 
-function process_guard_times(guard_times)
+function process_guard_times_a(guard_times)
     max_time = -1
     max_id = -1
     for (guard_id, guard_time) in guard_times
@@ -58,12 +55,37 @@ function process_guard_times(guard_times)
     max_id * (findmax(guard_times[max_id])[2] - 1)
 end
 
+function process_a(sched)
+    guard_times = process_schedule(sched)
+    process_guard_times_a(guard_times)
+end
 
-sched = parse_input(load("input_a.txt"))
+function run_a()
+    sched = parse_input()
+    process_a(sched)
+end
 
-guard_times = process_schedule(sched)
+function process_guard_times_b(guard_times)
+    max_time = (-1, -1)
+    max_id = -1
+    for (guard_id, guard_time) in guard_times
+        total_time = findmax(guard_time)
+        if total_time[1] > max_time[1]
+            max_id = guard_id
+            max_time = total_time
+        end
+    end
+    max_id * (max_time[2] - 1)
+end
 
-result = process_guard_times(guard_times)
+function process_b(sched)
+    guard_times = process_schedule(sched)
+    process_guard_times_b(guard_times)
+end
 
-println(result)
+function run_b()
+    sched = parse_input()
+    process_b(sched)
+end
 
+end # module
